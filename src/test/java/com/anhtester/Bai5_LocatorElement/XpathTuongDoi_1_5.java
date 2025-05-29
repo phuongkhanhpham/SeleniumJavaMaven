b11fc65599bf12d493267242fe2941dedc14b9da
Khanh Pham <khanhpham7003@gmail.com>
2025-05-29 17:34:42 +0700

    Add Xpath tuong doi 1-5

---
+++ src/test/java/com/anhtester/Bai5_LocatorElement/XpathTuongDoi_1_5.java
@@ -0,0 +1,28 @@
+package com.anhtester.Bai5_LocatorElement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class XpathTuongDoi_1_5 {
+    public static void main(String[] args) {
+
+        WebDriver driver;
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://crm.anhtester.com/admin/authentication");
+
+        System.out.println(driver.findElement(By.xpath("//h1[normalize-space()='Login']")).getText());
+
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("admin@example.com");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123456");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        driver.quit();
+
+    }
+}
